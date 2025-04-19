4da5f025c7f4006c93a50fa7b7a626ba025a3682
GabrielSilva <gabrielbebesilva247@gmail.com>
2025-04-19 20:46:34 -0300

    Adionada a função de desablitar o botão ler quando o campo de texto estiver vazio.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const botaoLer = document.querySelector("#ler");
 const campoTexto = document.querySelector("#texto");
 
-botaoLer.addEventListener("click", () => { 
+botaoLer.disabled = true; // Desabilita o botão inicialmente
+
+campoTexto.addEventListener("input", () => {
+  botaoLer.disabled = campoTexto.value.trim() === "";
+}); // Habilita o botão quando há texto no campo
+
+botaoLer.addEventListener("click", () => {
   const texto = campoTexto.value;
   const fala = new SpeechSynthesisUtterance(texto);
+  
+  fala.lang = "pt-BR";
   speechSynthesis.speak(fala);
 });
-
