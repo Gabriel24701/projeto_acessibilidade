d0894c133a0ece41e2216b0b748d230e566b6dbd
GabrielSilva <gabrielbebesilva247@gmail.com>
2025-04-19 21:52:12 -0300

    Adicionada a funcionabilidade do usário usar o botão "ler", usando a tecla "enter".

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const botaoLer = document.querySelector("#ler");
-const campoTexto = document.querySelector("#texto");
+const botaoLer = document.querySelector("#ler"); // Seleciona o botão "ler"
+const campoTexto = document.querySelector("#texto"); // Seleciona o campo de entrada de texto
 
 botaoLer.disabled = true; // Desabilita o botão inicialmente
 
@@ -8,9 +8,15 @@ campoTexto.addEventListener("input", () => {
 }); // Habilita o botão quando há texto no campo
 
 botaoLer.addEventListener("click", () => {
-  const texto = campoTexto.value;
-  const fala = new SpeechSynthesisUtterance(texto);
-  
-  fala.lang = "pt-BR";
-  speechSynthesis.speak(fala);
+  const texto = campoTexto.value; // Obtém o texto do campo de entrada
+  const fala = new SpeechSynthesisUtterance(texto); // Cria uma nova instância de SpeechSynthesisUtterance com o texto
+  fala.lang = "pt-BR"; // Define o idioma como português do Brasil
+  speechSynthesis.speak(fala); // Inicia a leitura do texto
 });
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault(); // Impede o comportamento padrão do Enter
+        botaoLer.click(); // Simula o clique no botão "ler
+    }
+})
